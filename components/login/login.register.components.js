b898dec8b0edab078b0cd48a2a9b4904f12aba04
Sefa Gür <sefa_guer@hotmail.com>
2023-11-21 12:33:34 +0100

    basic register structure

---
+++ components/login/login.register.components.js
@@ -0,0 +1,19 @@
+let users = [];
+
+
+
+
+
+
+async function init() {
+     // await getStorageData(key)
+}
+
+
+function register() {
+     let name = document.getElementById('Names').value;
+     let email = document.getElementById('mails').value;
+     let password = document.getElementById('password').value;
+     let passwordConfirm = document.getElementById('passwordConfirm').value;
+}
+
