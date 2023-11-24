38509de20e3ffd84d393ed72357526d484fb38b7
Sefa Gür <sefa_guer@hotmail.com>
2023-11-24 20:02:12 +0100

    register and login function finish

--- components/login/login.register.components.js
+++ components/login/login.register.components.js
@@ -1,11 +1,17 @@
-let users = [];
+let users = [
+     {
+          'email': 'sefa_guer@hotmail.com',
+          'password': 'asdasdasd'
+     }
+];
+
 
 async function init() {
      // await getStorageData(key)
 }
 
 
-function register() {
+async function register() {
 
      let name = document.getElementById('Name').value;
      let email = document.getElementById('mail').value;
@@ -18,26 +24,13 @@ function register() {
 
      } else {
           if (checkPassword(password, passwordConfirm)) {
-               users.push({ name: name, email: email, password: password })
-               openLogin();
-               // setStorageData(`${key}`, users)
+               users.push({ name: name, email: email, password: password });
+               // setStorageData(`${key}`, users);
+               await popup();
           } else {
                window.alert("password is incorrect");
           }
      }
-}
-
-
-function openLogin() {
-     window.location.href = '/components/login/login.html';
-     popup()
-}
-
-
-function back() {//from register back to login html
-
-     window.location.href = '/components/login/login.html';
-
 }
 
 
@@ -51,17 +44,60 @@ function checkPassword(password, passwordConfirm) {//check if both passwords are
 }
 
 
+function popup() {
+     let popupScreen = document.getElementById('popup');
+     popupScreen.classList.remove('d-none');
+     setTimeout(() => {
+          openLogin();
+     }, 2000);
+}
+
+
+function openLogin() {
+     window.location.href = '/components/login/login.html';
+}
+
+
+function back() {//from register back to login html
+     window.location.href = '/components/login/login.html';
+}
+
+
 //////////////////////////////////////////////////////////////////////////// LOG IN /////////////////////////////////////////////////////////////////////////////////////////
 
+
+function login() {
+
+     let email = document.getElementById('mail').value;
+     let password = document.getElementById('password').value;
+
+     if (checkEmailLogin(email)) {
+          if (checkPasswordLogin(password)) {
+               console.log('du bist eingeloggt')
+          } else {
+               window.alert("password is incorrect");
+          }
+     } else {
+          window.alert("Email is incorrect");
+     }
+}
+
+
+function checkEmailLogin(email) {//check if email is already in useer
+
+     return users.findIndex(users => users['email'] == email) > -1;
+
+}
+
+
+function checkPasswordLogin(password) {//check if  password is correct
+
+     return users.findIndex(users => users['password'] == password) > -1;
+}
 
 
 function signup() {//from login to register html
 
      window.location.href = '/components/login/register.html';
 
-}
-
-
-function popup() {
-     document.getElementById('popup').classList.remove('d-none');
 }
