08d3939ea306e5a904a8513e9644fac14d5d4e66
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2023-12-18 16:27:35 +0100

    began merging

--- components/contacts/contacts.js
+++ components/contacts/contacts.js
@@ -1,14 +1,3 @@
-let ACTIVE_USER = '738927';
-
-let userContacts = [ 
-    {"name": "Benedikt Ziegler", "email": "benediktz@gmail.com", "phone": "+1234567", "contactId": "98765abc", "initials": "BZ", "color": "812731"},
-    {"name": "Anton Mayer", "email": "antom@gmail.com","phone": "+1234567", "contactId": "12345abc", "initials": "AM", "color": "3e59c2"},
-    {"name": "Helena Eissele", "email": "helenae@gmail.com","phone": "+1234567", "contactId": "97345oiu", "initials": "HE", "color": "2b3430"},
-    {"name": "Izak Abraham", "email": "izaka@gmail.com","phone": "+1234567", "contactId": "12367oiu", "initials": "IA", "color": "907ee1"},
-    {"name": "Anja Schulz", "email": "anjas@gmail.com","phone": "+1234567", "contactId": "12345ghf", "initials": "AS", "color": "3e59c2"},
-    {"name": "David Eisenberg", "email": "davide@gmail.com","phone": "+1234567", "contactId": "12345oiu", "initials": "DE", "color": "4f98ce"}
-]; 
-
 const abcString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ'; 
 
 let activeContact = null;
--- components/tasks/tasks.js
+++ components/tasks/tasks.js
@@ -1,11 +1,4 @@
-let userContacts = [ 
-    {"name": "Benedikt Ziegler", "email": "benediktz@gmail.com", "phone": "+1234567", "contactId": "98765abc", "initials": "BZ", "color": "#812731"},
-    {"name": "Anton Mayer", "email": "antom@gmail.com","phone": "+1234567", "contactId": "12345abc", "initials": "AM", "color": "#3e59c2"},
-    {"name": "Helena Eissele", "email": "helenae@gmail.com","phone": "+1234567", "contactId": "97345oiu", "initials": "HE", "color": "#2b3430"},
-    {"name": "Izak Abraham", "email": "izaka@gmail.com","phone": "+1234567", "contactId": "12367oiu", "initials": "IA", "color": "#907ee1"},
-    {"name": "Anja Schulz", "email": "anjas@gmail.com","phone": "+1234567", "contactId": "12345ghf", "initials": "AS", "color": "#3e59c2"},
-    {"name": "David Eisenberg", "email": "davide@gmail.com","phone": "+1234567", "contactId": "12345oiu", "initials": "DE", "color": "#4f98ce"}
-]; 
+
 // let userContactsClone = USERS.ACTIVEUSER.userContacts; 
 
 let assignedToTask = [];
@@ -53,8 +46,8 @@ function pushTask(dateCreated, id, title, description, user, date, prio, categor
 
 function pushUSERS(){
     USERS.ACTIVEUSER.tasks = tasks
+    // USERS.Guest.tasks = tasks
 }
-
 
 function resetTask() {
     document.getElementById('task_title').value = '';
