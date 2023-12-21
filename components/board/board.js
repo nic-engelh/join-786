05ae2e23203f91cf038cd69c3fc98e1db730685c
Niklas <110847147+nic-engelh@users.noreply.github.com>
2023-12-21 11:44:15 +0100

    changed function naming within board.js for more readability

--- components/board/board.js
+++ components/board/board.js
@@ -10,6 +10,8 @@ let todos = [{
     'id': 0,
 },];
 
+
+
 //let toDos = USERS[ACTIVEUSERKEY].tasks;
 
 let currentDraggedElement;
@@ -40,10 +42,10 @@ function updateToDo () {
     let todo = todos.filter(t => t['status'].toLowerCase().replaceAll(" ","") == 'todo');
     document.getElementById('todo').innerHTML = '';
     if (todo.length == 0) {
-        noTask('todo');
+        showNoTasksDone('todo');
     } else {
-        task('todo');
-        forFunction(todo, 'todo');
+        showTask('todo');
+        renderingBoardTasks(todo, 'todo');
     }
 }
 
@@ -56,10 +58,10 @@ function updateInProgress () {
     let progress = todos.filter(t => t['status'].toLowerCase().replaceAll(" ","") == 'inprogress');
     document.getElementById('inProgress').innerHTML = '';
     if (progress.length == 0) {
-        noTask('inProgress');
+        showNoTasksDone('inProgress');
     } else {
-        task('inProgress');
-        forFunction(progress, 'inProgress');
+        showTask('inProgress');
+        renderingBoardTasks(progress, 'inProgress');
     }
 }
 
@@ -71,10 +73,10 @@ function updateFeedback () {
     let feedback = todos.filter(t => t['status'].toLowerCase().replaceAll(" ","") == 'feedback');
     document.getElementById('feedback').innerHTML = '';
     if (feedback.length == 0) {
-        noTask('feedback');
+        showNoTasksDone('feedback');
     } else {
-        task('feedback');
-        forFunction(feedback, 'feedback');
+        showTask('feedback');
+        renderingBoardTasks(feedback, 'feedback');
     }
 }
 
@@ -86,10 +88,10 @@ function updateDone () {
     let done = todos.filter(t => t['status'].toLowerCase().replaceAll(" ","") == 'done');
     document.getElementById('done').innerHTML = '';
     if (done.length == 0) {
-        noTask('done');
+        showNoTasksDone('done');
     } else {
-        task('done');
-        forFunction(done, 'done');
+        showTask('done');
+        renderingBoardTasks(done, 'done');
     }
 }
 
@@ -111,7 +113,7 @@ function generateTodoHTML(element) {
     return `
     <div class ="todo">
        <div draggable="true" ondragstart="startDragging(${element['id']})" class="taskToDo">
-          <div class="userHeadline">${element['headline']}</div>
+          <div class="userHeadline">${element['category']}</div>
           <div class="title">${element['title']}</div>
           <div class="description">${element['description']}</div>
            <div class="progressPosition">
@@ -151,7 +153,7 @@ function highlight(id) {
 }
 
 /**
- * remove a class
+ * remove the class "drag-area-highlight"
  * @param {string} id 
  */
 function removeHighlight(id) {
@@ -159,11 +161,12 @@ function removeHighlight(id) {
 }
 
 /**
- * for schleife for tasks
+ * for schleife for tasks to rendering board elements html
+ * 
  * @param {array} id 
  * @param {string} name 
  */
-function forFunction(id, name) {
+function renderingBoardTasks(id, name) {
     for (let index = 0; index < id.length; index++) {
         const element = id[index];
         document.getElementById(`${name}`).innerHTML += generateTodoHTML(element);
@@ -171,18 +174,20 @@ function forFunction(id, name) {
 }
 
 /**
- * remove and add class
+ * remove and add class for information that no tasks are done 
+ * 
  * @param {string} id 
  */
-function noTask(id) {
+function showNoTasksDone(id) {
     document.getElementById(`${id}`).classList.add('noTask')
-    document.getElementById(`${id}`).innerHTML = 'No tasks in done'
+    document.getElementById(`${id}`).innerHTML = 'No tasks are done'
 }
 
 /**
- * remove classes
+ * remove class "no tasks" don at the board
+ * 
  * @param {string} id 
  */
-function task(id) {
+function showTask(id) {
     document.getElementById(`${id}`).classList.remove('noTask')
 }
