8213fbe6bf719864d0cc5a0c68f355a6eceb9ef2
Niklas <110847147+nic-engelh@users.noreply.github.com>
2023-12-22 14:01:08 +0100

    Added functions to j360 to show urgent Deadline

--- components/join360/join360.js
+++ components/join360/join360.js
@@ -1,51 +1,55 @@
 function init() {
-    updateBoardHTML();
+    updateDashBoardHTML();
 }
 
 /**
  * main functions for updating the drag & drop fields for tasks objects
  */
-function updateBoardHTML() {
+function updateDashBoardHTML() {
     setVariables();
-    updateToDoField();
-    updateInProgressField();
-    updateFeedbackField();
-    updateDoneField();
+    updateToDoWidget();
+    updateInProgressWidget();
+    updateFeedbackWidget();
+    updateDoneWidget();
 }
 
 /**
  * function filters all tasks with status todo from active user tasks object
  * 
  */
-function updateToDoField() {
+function updateToDoWidget() {
     let todo = document.getElementById('j36_todo');
     todo.innerHTML = ``;
     let targetValue = 'todo';
-    let inputObject = userTasks;
-    let targetKey = 'status';
-    let toDoTasks = filterNestedObject(inputObject, targetValue, targetKey);
+    let toDoTasks = getFilteredTasksByStatus(targetValue);
     let size = Object.keys(toDoTasks).length;
-    todo.innerHTML = `<div onclick="myfunction()" class="j36_toDo">
-    <img src="/assets/img/Group 7.png" alt="">
-    <div class="j36_numberPosition">
-        <b>${size}</b>
-        <p id="j36_todo" class="j36_todoText">To-Do</p>
-    </div>`;
+    todo.innerHTML = generateToDoWidget(size);
+}
+
+function generateToDoWidgetHTML (size) {
+    return /*html*/`
+    <div onclick="openSection('sectionBoard')" class="j36_toDo">
+        <img src="/assets/img/Group 7.png" alt="">
+        <div class="j36_numberPosition">
+            <b>${size}</b>
+            <p id="j36_todo" class="j36_todoText">To-Do</p>
+    </div>
+    `;
 }
 
 /**
  * function filters all tasks with status "inprogress" from active user tasks object
  * 
  */
-function updateInProgressField() {
+function updateInProgressWidget() {
     let inprogress = document.getElementById('j36_progress');
     inprogress.innerHTML = ``;
     let targetValue = 'inprogress';
-    let targetKey = 'status';
-    let progressTasks = filterNestedObject(inputObject, targetValue, targetKey);
-    //let progress = userTasks.filter(t => t['status'].toLowerCase().replaceAll(" ","") == 'inprogress');
+    let progressTasks = getFilteredTasksByStatus(targetValue);
     let size = Object.keys(progressTasks).length;
-    inprogress.innerHTML = `<b id="j36_progress" class="j36_numberOfTasks">${size}</b>
+    inprogress.innerHTML = 
+    
+    `<b id="j36_progress" class="j36_numberOfTasks">${size}</b>
     <p>Task in Progress</p>`;
 }
 
@@ -53,13 +57,11 @@ function updateInProgressField() {
  * function filters all taks with status "feedback" from active user tasks object
  * 
  */
-function updateFeedbackField() {
+function updateFeedbackWidget() {
     let feedback = document.getElementById('j36_feedback');
     feedback.innerHTML = ``;
     let targetValue = 'feedback';
-    let inputObject = userTasks;
-    let targetKey = 'status';
-    let feedbackTasks = filterNestedObject(inputObject, targetValue, targetKey);
+    let feedbackTasks =  getFilteredTasksByStatus(targetValue);
     let size = Object.keys(feedbackTasks).length;
     feedback.innerHTML = `  <b class="j36_numberOfTasks">${size}</b>
     <p>Awaiting Feedback</p>`;
@@ -69,13 +71,10 @@ function updateFeedbackField() {
  * function filters all tasks with status "done" from active user tasks object
  * 
  */
-function updateDoneField() {
+function updateDoneWidget() {
     let done = document.getElementById('j36_done');
     done.innerHTML = ``;
-    let targetValue = 'done';
-    let inputObject = userTasks;
-    let targetKey = 'status';
-    let doneTasks = filterNestedObject(inputObject, targetValue, targetKey);
+    let doneTasks = getFilteredTasksByStatus ("done");
     let size = Object.keys(doneTasks).length;
     done.innerHTML = `  <img src="/assets/img/Group 7 (1).png" alt="">
     <div class="j36_numberPosition">
@@ -84,32 +83,59 @@ function updateDoneField() {
     </div>`;
 }
 
-//der task der gerade am meisten eilt und aktueller datum bis wan es fertig sein musss
-function updateUrgentField() {
-    let urgent = document.getElementById('j36_Urgent');
-    urgent.innerHTML = ``;
-    let targetValue = 'done';  //urgent
+
+/**
+ * functions searches userTasks for objects with status as target value
+ * 
+ * @param {string} targetValue 
+ * @returns filtered object with tasks with status targetValue
+ */
+function getFilteredTasksByStatus (targetValue) {
     let inputObject = userTasks;
     let targetKey = 'status';
-    let doneTasks = filterNestedObject(inputObject, targetValue, targetKey);
-    let size = Object.keys(doneTasks).length;
-    urgent.innerHTML = ` <div onclick="myfunction()" class="j36_urgent">
-    <img class="j36_image1" src="/assets/img/Ellipse 4.jpg" alt="">
-    <img class="j36_image2" src="/assets/img/Prio alta.jpg" alt="asdasd">
-    <div class="j36_numberPosition">
-        <b>${size}</b>
-        <p class="j36_todoText">Urgent</p>
-    </div>
-    <div class="j36_partingLine"></div>
-    <div class="j36_Date">
-        <h2>October 16, 2022</h2>        <---------------datum bis wan es fertig sein muss///////////////////////////
-        <p class="j36_dateText">Upcoming Deadline</p>
-    </div>`;
+    let filteredObject = filterNestedObject(inputObject, targetValue, targetKey);
+    return filteredObject;
 }
 
+
+// TODO cleaning and refactoring
+function updateUrgentWidget() {
+    let urgent = document.getElementById('j36_Urgent');
+    urgent.innerHTML = ``;
+    let targetValue = 'urgent';  //urgent
+    let urgentTasks = Object.keys( filterNestedObject(userTasks, targetValue, 'prio') );
+    let deadlineDate = urgentTasks[0].date; // urgentTasks[key].date
+    for (const task of urgentTasks ){
+        let nextDate = task.date;
+        if (nextDate < deadlineDate) {
+            deadlineDate = nextDate;
+        }
+     };
+    urgent.innerHTML = generateUrgentWidgetHTML(size, deadlineDate);
+}
+
+function generateUrgentWidgetHTML(size, deadlineDate) {
+    return /*html*/` 
+        <div onclick="myfunction()" class="j36_urgent">
+        <img class="j36_image1" src="/assets/img/Ellipse 4.jpg" alt="">
+        <img class="j36_image2" src="/assets/img/Prio alta.jpg" alt="asdasd">
+        <div class="j36_numberPosition">
+            <b>${size}</b>
+            <p class="j36_todoText">Urgent</p>
+        </div>
+        <div class="j36_partingLine"></div>
+        <div class="j36_Date">
+            <h2>${deadlineDate}</h2>  
+            <p class="j36_dateText">Upcoming Deadline</p>
+        </div>
+    `
+}
+
+
 // komplette task in board plus rechenen und returnen
-function updateBoardField() {
+function updateBoardWidget() {
     let board = document.getElementById('j36_board');
+    // size muss berechnet werden aus länge des key.userTasks
     board.innerHTML = ``;
     board.innerHTML = `  <img src="/assets/img/Group 7 (1).png" alt="">
     <div class="j36_numberPosition">
