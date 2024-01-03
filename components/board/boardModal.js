93cabc284dbea753e2d38d3299ad4d8d69f5423b
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-01-03 11:58:47 +0100

    added succes info modal for deleting task function

--- components/board/boardModal.js
+++ components/board/boardModal.js
@@ -463,7 +463,7 @@ async function boardModalDeleteTask() {
     ID = null;
     closeBoardModal();
     await setStorageData("user", USERS);
-    showSuccessInfo(4); 
+    showSuccessInfo("4"); 
     // show modal task deleted
     // TODO set local user storage
 }
