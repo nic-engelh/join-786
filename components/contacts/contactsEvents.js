cc772ef207c38351fd5f17a8216bead14d09355e
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-01-14 22:27:09 +0100

    added event listener function for checking screen size

--- components/contacts/contactsEvents.js
+++ components/contacts/contactsEvents.js
@@ -9,3 +9,34 @@
 // listen for innerwidth with changes event
 // if innerwidth is < 1000 px activate modal mobile and close modal desktop
 // if innderwidth is > 1000 px activete modal desktop and close modal mobile
+
+
+/**
+ * function for check window size and controlling modals
+ * 
+ */
+function checkScreenSize() {
+    // Bildschirmbreite abrufen
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  
+    // Überprüfen, ob die Bildschirmbreite größer als 1000px ist
+    if (screenWidth > 1000) {
+      // Modal_a schließen (falls es geöffnet ist)
+      var modalA = document.getElementById('modal_a');
+      if (modalA) {
+        modalA.style.display = 'none';
+      }
+  
+      // Modal_b öffnen (falls vorhanden)
+      var modalB = document.getElementById('modal_b');
+      if (modalB) {
+        modalB.style.display = 'block';
+      }
+    }
+  }
+  
+  // Eventlistener hinzufügen, um die Funktion bei Änderungen der Bildschirmgröße aufzurufen
+  window.addEventListener('resize', checkScreenSize);
+  
+  // Die Funktion beim Laden der Seite einmalig aufrufen, um den initialen Status zu überprüfen
+  window.addEventListener('load', checkScreenSize);
