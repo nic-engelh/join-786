5aee0776c44ffb56ef2cc887eec2930e36d91504
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-01-14 22:22:22 +0100

    created new files for event listener functions

---
+++ components/contacts/contactsEvents.js
@@ -0,0 +1,11 @@
+
+// event listener 1:
+// check if section join36, board, add tasks or contacts are active
+// if so remove contact-view-profil-main
+
+
+// event listener 2:
+// check if modal (desktop or mobile) is active
+// listen for innerwidth with changes event
+// if innerwidth is < 1000 px activate modal mobile and close modal desktop
+// if innderwidth is > 1000 px activete modal desktop and close modal mobile
