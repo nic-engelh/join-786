a9eb8d18d822c061997c4dc38db39cbfcdfa125f
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-01-17 16:56:46 +0100

    fix link

--- components/login/register.components.js
+++ components/login/register.components.js
@@ -76,14 +76,14 @@ function popup() {
  * leads you to login html
  */
 function openLogin() {
-     window.location.href = '/components/login/login.html';
+     window.location.href = 'http://gruppe-786.developerakademie.net/join-786/components/login/login.html';
 }
 
 /**
  * /from register back to login html
  */
 function back() {
-     window.location.href = '/components/login/login.html';
+     window.location.href = 'http://gruppe-786.developerakademie.net/join-786/components/login/login.html';
 }
 
 /**
